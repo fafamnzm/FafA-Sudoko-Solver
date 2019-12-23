d0476b3431fde481f0a6ebd1c70d605fc1a9a932
faramarz monazami haghighat <51343277+fafamnzm@users.noreply.github.com>
2019-12-23 04:50:46 +0330

    Modifications
    It is prettier a little!

--- SudokoSolver.py
+++ SudokoSolver.py
@@ -1,4 +1,6 @@
-original = []
+import time
+
+""" original = []
 original.append([8, 0, 0, 7, 1, 0, 0, 0, 0])
 original.append([3, 2, 7, 0, 0, 0, 0, 0, 0])
 original.append([0, 0, 1, 0, 2, 9, 8, 7, 6])
@@ -7,15 +9,47 @@ original.append([0, 0, 0, 5, 9, 0, 0, 8, 2])
 original.append([7, 8, 2, 0, 3, 6, 0, 1, 0])
 original.append([9, 0, 5, 2, 6, 4, 7, 3, 0])
 original.append([0, 0, 0, 0, 0, 1, 2, 6, 9])
-original.append([0, 0, 0, 0, 0, 3, 1, 5, 4])
+original.append([0, 0, 0, 0, 0, 3, 1, 5, 4]) """
+""" original = []
+original.append([3, 0, 6, 5, 0, 8, 4, 0, 0])
+original.append([5, 2, 0, 0, 0, 0, 0, 0, 0])
+original.append([0, 8, 7, 0, 0, 0, 0, 3, 1])
+original.append([0, 0, 3, 0, 1, 0, 0, 8, 0])
+original.append([9, 0, 0, 8, 6, 3, 0, 0, 5])
+original.append([0, 5, 0, 0, 9, 0, 6, 0, 0])
+original.append([1, 3, 0, 0, 0, 0, 2, 5, 0])
+original.append([0, 0, 0, 0, 0, 0, 0, 7, 4])
+original.append([0, 0, 5, 2, 0, 6, 3, 0, 0])
+ """
+""" checker = []
+checker.append([8, 0, 0, 7, 1, 0, 0, 0, 0])
+checker.append([3, 2, 7, 0, 0, 0, 0, 0, 0])
+checker.append([0, 0, 1, 0, 2, 9, 8, 7, 6])
+checker.append([6, 5, 0, 0, 0, 0, 0, 0, 0])
+checker.append([0, 0, 0, 5, 9, 0, 0, 8, 2])
+checker.append([7, 8, 2, 0, 3, 6, 0, 1, 0])
+checker.append([9, 0, 5, 2, 6, 4, 7, 3, 0])
+checker.append([0, 0, 0, 0, 0, 1, 2, 6, 9])
+checker.append([0, 0, 0, 0, 0, 3, 1, 5, 4]) """
+original = []
+original.append([0, 0, 3, 0, 7, 0, 0, 0, 5])
+original.append([0, 0, 0, 5, 0, 4, 9, 8, 7])
+original.append([0, 0, 0, 0, 0, 0, 2, 0, 0])
+original.append([2, 0, 4, 0, 0, 0, 0, 0, 0])
+original.append([0, 6, 7, 0, 5, 0, 0, 0, 8])
+original.append([3, 1, 0, 0, 0, 0, 0, 6, 0])
+original.append([0, 0, 1, 8, 4, 0, 0, 5, 0])
+original.append([0, 0, 0, 2, 0, 1, 0, 4, 0])
+original.append([0, 7, 0, 0, 0, 0, 0, 0, 0])
 
 
 checker = original
 
 count = 0
+startTime = time.time()
 
 def finisher(tbl):
-    if count > 100:
+    if count > 5000:
         return False
     
     for row in range(9):
@@ -32,6 +66,7 @@ for row in range(9):
 
 while finisher(checker):    
     for i in range(0, 81):
+        repeaterBr = 0
         row = int( i / 9)
         column = i % 9
         if type(original[row][column]) == set:
@@ -95,7 +130,13 @@ while finisher(checker):
             if len(checker[row][column]) == 1 :
                 original[row][column] = int(list( checker[row][column] )[0])
 #    print(original)
-print(count)
+endTime = time.time()
+
 for line in checker:
     print(line)
 
+print()
+print("No. Count = %d" %count)
+print()
+print("Elapsed Time = {} Seconds".format(endTime - startTime))
+print()
